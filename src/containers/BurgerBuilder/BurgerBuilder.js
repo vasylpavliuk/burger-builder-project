a9694c4f3aaf05764b42ab06bab96db10c565085
vasylpavliuk <vasylpavliuk007@gmail.com>
2020-06-10 00:52:57 +0300

    Added connection to the server with axios package

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -17,12 +18,7 @@ class BurgerBuilder extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ingredients: {
-        salad: 0,
-        bacon: 0,
-        cheese: 0,
-        meat: 0,
-      },
+      ingredients: null,
       totalPrice: 2,
       purchaseable: false,
       purchasing: false,
@@ -34,6 +30,14 @@ class BurgerBuilder extends Component {
     this.purchaseHandler = this.purchaseHandler.bind(this);
     this.purchaseCancelHandler = this.purchaseCancelHandler.bind(this);
     this.purchaseContinueHandler = this.purchaseContinueHandler.bind(this);
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://react-my-burger-d8363.firebaseio.com/ingredients.json")
+      .then((response) => {
+        this.setState({ ingredients: response.data });
+      });
   }
 
   updatePurchaseState(ingredients) {
@@ -74,7 +78,23 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler() {
-    alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Vasyl Pavliuk",
+        address: {
+          street: "Example Str.",
+          country: "Ukraine",
+        },
+        email: "PavlyukV@i.ua",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => this.setState({ purchasing: false }))
+      .catch((error) => this.setState({ purchasing: false }));
   }
 
   render() {
@@ -88,27 +108,31 @@ class BurgerBuilder extends Component {
 
     return (
       <Auxiliary>
-        <Modal
-          show={this.state.purchasing}
-          modalClosed={this.purchaseCancelHandler}
-        >
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler}
-          />
-        </Modal>
-        <Burger ingredients={this.state.ingredients} />
-        <BuildControls
-          ingredients={this.state.ingredients}
-          addIngredient={this.addIngredientHandler}
-          deductIngredient={this.deductIngredientHandler}
-          disabled={disabledInfo}
-          price={this.state.totalPrice}
-          ordered={this.purchaseHandler}
-          purchaseable={this.state.purchaseable}
-        />
+        {this.state.ingredients !== null ? (
+          <Auxiliary>
+            <Modal
+              show={this.state.purchasing}
+              modalClosed={this.purchaseCancelHandler}
+            >
+              <OrderSummary
+                ingredients={this.state.ingredients}
+                price={this.state.totalPrice}
+                purchaseCancelled={this.purchaseCancelHandler}
+                purchaseContinued={this.purchaseContinueHandler}
+              />
+            </Modal>
+            <Burger ingredients={this.state.ingredients} />
+            <BuildControls
+              ingredients={this.state.ingredients}
+              addIngredient={this.addIngredientHandler}
+              deductIngredient={this.deductIngredientHandler}
+              disabled={disabledInfo}
+              price={this.state.totalPrice}
+              ordered={this.purchaseHandler}
+              purchaseable={this.state.purchaseable}
+            />
+          </Auxiliary>
+        ) : null}
       </Auxiliary>
     );
   }
